9b7553dbae35609301b6b6750c068c207fb3a00a
Lucas Gabriel <lucasogjc@gmail.com>
2022-06-26 10:46:39 -0300

    adds new assertions to add item test method and removes comments

--- src/test/java/demogradle/cart/CartTest.java
+++ src/test/java/demogradle/cart/CartTest.java
@@ -18,16 +18,13 @@ public class CartTest {
     @Test
     public void getTotalPrice()
     {
-        //Arrange
         double total = 0.0;
         cart.addItem(new Item("Batata", 5.50));
         cart.addItem(new Item("Arroz", 10.20));
         cart.addItem(new Item("Banana", 2.30));
 
-        //Act
         total = cart.getTotalPrice();
 
-        //Assert
         Assertions.assertNotEquals(0.0, total);
         Assertions.assertEquals(18.00, total);
     }
@@ -35,11 +32,15 @@ public class CartTest {
     @Test
     public void addItem()
     {
-        //Act
-        cart.addItem(new Item("Batata", 5.50));
+        Item item = new Item("Batata", 5.50);
 
-        //Assert
+        cart.addItem(item);
+
+        Assertions.assertDoesNotThrow(() -> {
+            cart.addItem(item);
+        });
         Assertions.assertFalse(cart.getItems().isEmpty());
+        Assertions.assertEquals(2, cart.getItems().size());
     }
 
     @Test
@@ -53,15 +54,12 @@ public class CartTest {
     @Test
     public void getPurchasePossibility()
     {
-        //Arrange
         double purchasePossibility = 0.0;
         cart.addItem(new Item("Batata", 25.50));
         cart.addItem(new Item("Arroz", 14.50));
 
-        //Act
         purchasePossibility = cart.getPurchasePossibility();
 
-        //Assert
         Assertions.assertEquals(60.00, purchasePossibility);
     }
 }
